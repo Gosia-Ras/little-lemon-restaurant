e4144dbfdc3248bbc52078594a8633eaad91049f
Gosia Hildebrand <ras.malgorzata@gmail.com>
2024-04-04 16:02:56 +0200

    Corrected the footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,13 +4,13 @@ import "../styles/Footer.css";
 
 function Footer() {
   return (
-    <footer>
+    <footer id="footer">
       <img src={logo} alt="Little Lemon Logo" />
-      <div>
+      <div className="nav">
         <h3>Navigation</h3>
         <Nav />
       </div>
-      <div>
+      <div className="address">
         <address>
           <h3>Contact Us</h3>
           <p>
@@ -23,19 +23,19 @@ function Footer() {
           <a href="mailto:info@littlelemon.com">info@littlelemon.com</a>
         </address>
       </div>
-      <div>
+      <div className="connect">
         <h3>Connect</h3>
         <ul>
           <li>
             {" "}
-            <a href="#">Facebook</a>
+            <a href="https://www.facebook.com">Facebook</a>
           </li>
           <li>
             {" "}
-            <a href="#">Instagram</a>
+            <a href="https://www.instagram.com">Instagram</a>
           </li>
           <li>
-            <a href="#">Join Us</a>
+            <a href="/booking-details">Join Us</a>
           </li>
         </ul>
       </div>
