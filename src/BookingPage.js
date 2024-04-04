7ef26cee95974ca5097c957956d45cd035499b1e
Gosia Hildebrand <ras.malgorzata@gmail.com>
2024-04-04 10:04:10 +0200

    Finished booking page

--- src/BookingPage.js
+++ src/BookingPage.js
@@ -1,18 +1,15 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
-import Menu from "./components/Menu";
-import Hero from "./components/Hero";
-import Testimonials from "./components/Testimonials";
+
+import BookingForm from "./components/BookingForm";
 
 function BookingPage() {
   return (
     <>
       <Header />
       <Main>
-        <Hero />
-        <Menu />
-        <Testimonials />
+        <BookingForm />
       </Main>
       <Footer />
     </>
---
+++ src/components/BookingForm.js
@@ -0,0 +1,113 @@
+import React from "react";
+import { useNavigate } from "react-router-dom"; //
+
+import "../styles/BookingForm.css";
+import bruschetta from "../assets/bruschetta.jpg";
+
+function BookingForm() {
+  const navigate = useNavigate(); //
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    navigate("/booking-details");
+  };
+  return (
+    <section className="booking">
+      <h1>Find your table</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="date">
+            Date<span className="required"> *</span>
+          </label>
+          <input type="date" id="date" name="date" required />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="time">
+            Time <span className="required"> *</span>
+          </label>
+          <select name="time">
+            <option>15:00</option>
+            <option>15:30</option>
+            <option>16:00</option>
+            <option>16:30</option>
+            <option>17:00</option>
+            <option>17:30</option>
+            <option>18:00</option>
+            <option>18:30</option>
+            <option>19:00</option>
+            <option>19:30</option>
+            <option>20:00</option>
+            <option>20:30</option>
+            <option>21:00</option>
+            <option>21:30</option>
+          </select>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="guests">
+            Guests <span className="required"> *</span>
+          </label>
+          <select>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </select>
+        </div>
+
+        <div className="form-group table">
+          <span>
+            Table
+            <br />
+            (optional):
+          </span>
+
+          <div className="custom-radio-container">
+            <label className="custom-radio">
+              Inside
+              <input
+                type="radio"
+                id="inside"
+                name="tableLocation"
+                value="inside"
+                required
+              />
+              <span className="checkmark"></span>
+            </label>
+
+            <label className="custom-radio">
+              Event Zone
+              <input
+                type="radio"
+                id="event"
+                name="tableLocation"
+                value="event"
+              />
+              <span className="checkmark"></span>
+            </label>
+
+            <label className="custom-radio">
+              Outside
+              <input
+                type="radio"
+                id="outside"
+                name="tableLocation"
+                value="outside"
+              />
+              <span className="checkmark"></span>
+            </label>
+          </div>
+        </div>
+
+        <input type="submit" className="submit" value="Reserve" />
+      </form>
+      <div className="image-container">
+        <img src={bruschetta} alt="bruschetta" />
+      </div>
+    </section>
+  );
+}
+
+export default BookingForm;
