aa321ec31537f0a706b710d7fd7baa71ceb87712
Gosia Hildebrand <ras.malgorzata@gmail.com>
2024-04-04 16:02:29 +0200

    Added new routes and changed the naming

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import "./styles/index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./Homepage";
 import BookingPage from "./BookingPage";
-import ReservationPage from "./ReservationPage";
+import SignUpPage from "./SignUpPage";
 import ConfirmationPage from "./ConfirmationPage";
 import LoginPage from "./LoginPage";
-
+import DeliveryPage from "./DeliveryPage";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/booking" element={<BookingPage />} />
-        <Route path="/booking-details" element={<ReservationPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
         <Route path="/confirmation" element={<ConfirmationPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/delivery" element={<DeliveryPage />} />
       </Routes>
     </BrowserRouter>
   );
